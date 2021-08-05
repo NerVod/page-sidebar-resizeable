79fe2b4629f3e53d381d7de2eb38877a336bdbcf
Benjamin JEANNEROT <benjamin.jeannerot@gmail.com>
2021-08-05 15:31:49 +0200

    ajout fonction throttle

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const SIDEBAR_WIDTH= 'SidebarWidth';
+const SIDEBAR_WIDTH= "sidebarWidth";
 /**
  * transform element into a resizer handler
  *  
@@ -34,13 +34,42 @@ function resizer(element, cb) {
     
 }
 
-resizer(document.querySelector('.resizer'), function (x) {
+resizer(document.querySelector('.resizer'),
+ rafThrottle (function (x) {
     const sidebarWidth = x + "px";
     sessionStorage.setItem(SIDEBAR_WIDTH, 'sidebarWidth')
     document.body.style.setProperty("--sidebar", sidebarWidth);
-});
+})
+);
 
 const sidebarWidth = sessionStorage.getItem(SIDEBAR_WIDTH);
 if (sidebarWidth !== null) {
     document.body.style.setProperty("--sidebar", sidebarWidth);
 }
+
+function rafThrottle (callback) {
+  let requestId = null
+
+  let lastArgs
+
+  const later = (context) => () => {
+    requestId = null
+    callback.apply(context, lastArgs)
+  }
+
+  const throttled = function(...args) {
+    lastArgs = args;
+    if (requestId === null) {
+      requestId = requestAnimationFrame(later(this))
+    }
+  }
+
+  throttled.cancel = () => {
+    cancelAnimationFrame(requestId)
+    requestId = null
+  }
+
+  return throttled
+}
+
+export default rafThrottle
